a609633a3925333211fa9340b7429cb4a4d2b7fd
Luiz Gustavo C Setten <luizgustavosetten@hotmail.com>
2020-05-19 16:35:41 -0300

    Funcionando exceto euclidiana

--- knn.py
+++ knn.py
@@ -2,29 +2,29 @@ from collections import Counter
 from reader import Reader
 
 class KNN:
-  def __init__(self, nomeCjTreino, vetordeteste):
+  def __init__(self, nomeCjTreino, vetordeteste, k):
     self.nomeCjTreino = nomeCjTreino
     self.vetordeteste = vetordeteste
+    self.k = k
 
   def runKNN_DTW(self):
     reader = Reader(self.nomeCjTreino, self.vetordeteste)
 
     #Gasta muito processameto então o ideal é aproveitar os dados
     matrizDTW = reader.RunDTW()  
-
+    
     matrizDTW.sort(key=lambda tup: tup[1])
     ocorrencias = []
 
-    k = [1,3,5,10]
     resultado = []
 
-    for i in range(k.__len__()):
+    for i in range(self.k.__len__()):
       resultadotemp = []
-      for x in range(k[i]):
+      for x in range(self.k[i]):
         ocorrencias.append(matrizDTW[x][0])
       c = Counter(ocorrencias).most_common()
       # resultadotemp.append("K = " + str(k[i]))
-      resultadotemp.append(k[i])
+      resultadotemp.append(self.k[i])
       resultadotemp.append(c[0][0])
       resultado.append(resultadotemp)
 
@@ -38,16 +38,16 @@ class KNN:
 
     matrizEuclidiana.sort(key=lambda tup: tup[1])
     ocorrencias = []
-    k = [1,3,5,10]
+
     resultado = []
 
-    for i in range(k.__len__()):
+    for i in range(self.k.__len__()):
       resultadotemp = []
-      for x in range(k[i]):
+      for x in range(self.k[i]):
         ocorrencias.append(matrizEuclidiana[x][0])
       c = Counter(ocorrencias).most_common()
       # resultadotemp.append("K = " + str(k[i]))
-      resultadotemp.append(k[i])
+      resultadotemp.append(self.k[i])
       resultadotemp.append(c[0][0])
       resultado.append(resultadotemp)
     
--- main.py
+++ main.py
@@ -1,15 +1,45 @@
 from knn import KNN
 
-teste = open('teste.txt', 'r')
-arquivoTreino = 'aqr.txt'
+# teste = open('teste.txt', 'r')
+# arquivoTreino = 'aqr.txt'
+# k = [1,3,5,10]
 
-# testeLinha = teste.readline()
-for testeLinha in teste:
-  linhateste = testeLinha.split(" ")
-  print("Valor esperado: " + linhateste[0])
-  vetordeteste = list(map(float,linhateste[1:])) 
-  ResultadoObtido = KNN(arquivoTreino, vetordeteste).runKNN_DTW()
-  print(ResultadoObtido)
+for n in range(5):
+  print('Teste: ' + str(n))
+  teste = open('./particoes/ts' + str(n) + '.txt', 'r')
+  arquivoTreino = './particoes/cj' + str(n) + '.txt'
+  k = [1,3,5,10]
+  # testeLinha = teste.readline()
+  count = 0
+  accuracyDTW = 0
+  accuracyEuclidiana = 0
+  for testeLinha in teste:
+    count += 1
+    linhateste = testeLinha.split(" ")
+    # print("Valor esperado: " + linhateste[0])
+    vetordeteste = list(map(float,linhateste[1:])) 
 
-  ResultadoObtido2 = KNN(arquivoTreino, vetordeteste).runKNN_Euclidiana()
-  print(ResultadoObtido2)
+    ResultadoDTW = KNN(arquivoTreino, vetordeteste, k).runKNN_DTW()
+    # print(ResultadoDTW)
+
+    ResultadoEuclidiana = KNN(arquivoTreino, vetordeteste, k).runKNN_Euclidiana()
+    # print(ResultadoEuclidiana)
+
+    hitDTW = 0 
+    for i in ResultadoDTW:
+      if(int(linhateste[0]) == int(i[1])):
+        hitDTW += 1
+
+    accuracyDTW += hitDTW/ResultadoDTW.__len__()
+
+    hitEuclidiana = 0 
+    for i in ResultadoEuclidiana:
+      if(int(linhateste[0]) == int(i[1])):
+        hitEuclidiana  += 1
+
+    accuracyEuclidiana += hitEuclidiana/ResultadoEuclidiana.__len__()
+    print("Progresso: " + str(count*100/240) + "% teste " + str(n+1))
+    # print(" Accuracy DTW: " + str(accuracyDTW*100/count) + "%")
+    # print("Accuracy Euclidiana: " + str(accuracyEuclidiana*100/count) + "%")
+  # print("Accuracy DTW: " + str(accuracyDTW*100/count) + "%")
+  # print("Accuracy Euclidiana: " + str(accuracyEuclidiana*100/count) + "%")
